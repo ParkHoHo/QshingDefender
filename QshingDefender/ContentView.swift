e694929b53c3d5b9d9a7d7dedbfecd9a5e372645
parkHoHo <hohoho00@sch.ac.kr>
2023-11-13 13:51:16 +0900

    Initial Commit

---
+++ QshingDefender/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QshingDefender
+//
+//  Created by 박경호 on 11/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QshingDefender/QshingDefenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QshingDefenderApp.swift
+//  QshingDefender
+//
+//  Created by 박경호 on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct QshingDefenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
